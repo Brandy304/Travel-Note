1d4070c83ee43d46718103301277770b0495eaf6
Brandy304 <b01734499@studentmail.uws.ac.uk>
2025-04-14 18:23:47 +0100

    Update

--- public/service-worker.js
+++ public/service-worker.js
@@ -23,6 +23,7 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then(keys => 
       Promise.all(
+        // eslint-disable-next-line array-callback-return
         keys.map(key => {
           if (key !== CACHE_NAME) {
             return caches.delete(key);
